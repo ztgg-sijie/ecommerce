8988accc3f48d086d29690cefca7d3c3a501cb8c
douyouzhe <douyouzhe@gmail.com>
2022-01-11 22:11:28 -0800

    push

--- src/main/java/com/ztgg/ecommerce/dao/AreaDao.java
+++ src/main/java/com/ztgg/ecommerce/dao/AreaDao.java
@@ -3,6 +3,7 @@ package com.ztgg.ecommerce.dao;
 import java.util.List;
 import com.ztgg.ecommerce.entity.Area;
 
+
 public interface AreaDao{ // C Read U D
 	/**
 	 * this function returns a list of areas
